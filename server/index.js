77e32126343fdd3460cdc8d9dfe6d4b1f22dd77b
Louis <louis.earl.14@gmail.com>
2021-05-26 18:41:08 +1200

    Using react template for front end

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const server = require('./server')
-
-const PORT = process.env.PORT || 3000
-
-server.listen(PORT, function () {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
-})
