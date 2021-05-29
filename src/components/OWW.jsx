080845545f2e0f7a72a1d4ed4b9ce0beaa5cf839
Louis <louis.earl.14@gmail.com>
2021-05-29 18:24:09 +1200

    oww paralax scroll as array

--- src/components/OWW.jsx
+++ src/components/OWW.jsx
@@ -7,6 +7,7 @@ function OWW() {
   useEffect(() => {
     window.onscroll = () => scrollFunction()
     window.onresize = () => scrollFunction()
+    scrollFunction()
 
     return () => {
       window.onscroll = null
@@ -14,35 +15,50 @@ function OWW() {
     }
   }, [])
 
+  function clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max)
+}
+
   function scrollFunction() {
-    const scrollY = window.scrollY
+    let scrollY = window.scrollY
+    const halfScreenHeight = window.screen.height
 
-    // remove inline style to get base sizes 
-    const owwLogoElement = document.getElementById("oww-logo")
-    const frame01Element = document.getElementById("oww-frame-01")
+    const elementIDs = ["oww-logo", "oww-frame-01", "oww-ship"]
 
-    owwLogoElement.removeAttribute("style")
-    frame01Element.removeAttribute("style")
+    const elements = elementIDs.map((e) => {
+      const arr = Array(5)
+      // id name
+      arr[0] = e
 
-    const baseLogoHeight = window.getComputedStyle(owwLogoElement, null).getPropertyValue('height')
-    const baseFrame01Size = window.getComputedStyle(frame01Element, null).getPropertyValue('height')
+      // html element ref
+      arr[1] = document.getElementById(e) 
 
-    let logoHeight = parseInt(baseLogoHeight, 10) - (scrollY / 2.5)
-    let frame01Height = parseInt(baseFrame01Size, 10) - ((scrollY ) / 1.5)
+      // remove inline style to get base sizes 
+      arr[1].removeAttribute("style")
 
-    // let header01Size = baseHeaderSize
-    // if (scrollY > 100) {
-    //   header01Size = parseInt(baseHeaderSize, 10) - ((scrollY - 300) / 16)
-    // }
+      // base height 
+      arr[2] = window.getComputedStyle(arr[1], null).getPropertyValue('height')
 
-    if (logoHeight < 0) logoHeight = 0
-    if (frame01Height < 0) frame01Height = 0
+      // calculate height - todo scale instead
+      arr[3] = parseInt(arr[2], 10) - ((scrollY - arr[1].offsetTop) / 3)
+      arr[3] = clamp(arr[3], 0, 1000)
 
-    owwLogoElement.style.height = (logoHeight + "px")
-    owwLogoElement.style.marginTop = (scrollY / 0.466 + "px")
+      // apply height 
+      arr[1].style.height = arr[3] + "px"
 
-    frame01Element.style.height = (frame01Height + "px")
-    frame01Element.style.marginTop = ((scrollY + 500) / 2 + "px")
+      // calculate margin top 
+      arr[4] = (scrollY - arr[1].offsetTop + halfScreenHeight)/2
+      arr[4] = clamp(arr[4], 0, 1000)
+
+      // apply margin top
+      arr[1].style.marginTop = arr[4] + "px"
+
+      return arr
+
+    })
+
+
+    // owwLogoElement.style.transform = ("scale(" + 1 + ")" )
 
 
 
@@ -67,18 +83,18 @@ function OWW() {
 
       <div className="oww-content__wrapper" >
         <div className="oww-content__card">
-          <img className="oww-ship" src="./images/oww-ship.jpg" />
+          <img id="oww-ship" className="oww-ship" src="./images/oww-ship.jpg" />
         </div>
       </div>
 
-      <div className="oww-content__wrapper" >
+      {/* <div className="oww-content__wrapper" >
         <div className="oww-content__card">
           <h3>My Involvement</h3>
           <p>
             As the Creator and Developer of One Way Wellington, I've been responsible for everything from ideation to production and release. This solo project has been an exciting venture because I’ve pursued what I love, designing and programming video games, while taking the project through the entire production pipeline alongside Wellington-based client, WellingtonNZ.
           </p>
         </div>
-      </div>
+      </div> */}
 
     </div>
 
