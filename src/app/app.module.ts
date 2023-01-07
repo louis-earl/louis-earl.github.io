07ec0dd2328930cf5783cb151690bb6e4c3d5ecc
Louis <louis.earl.14@gmail.com>
2023-01-07 22:30:10 +1300

    Adds splash-screen-state service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenStateService } from './services/splash-screen-state.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    SplashScreenStateService
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule { }
