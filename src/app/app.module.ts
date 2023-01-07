46279e2eb74cf3d2dfdf3a5d25d09cc5f3dbe986
Louis <louis.earl.14@gmail.com>
2023-01-07 22:49:24 +1300

    Adds initial splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashScreenStateService } from './services/splash-screen-state.service';
+import { SplashComponent } from './components/splash/splash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule, 
