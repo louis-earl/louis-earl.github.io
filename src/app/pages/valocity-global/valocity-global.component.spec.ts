ef3e13d73ca55abfdedca48dc8f399862d9c6ddc
Louis <louis.earl.14@gmail.com>
2023-07-21 12:50:13 +1200

    Placeholder project page for valocity global

---
+++ src/app/pages/valocity-global/valocity-global.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ValocityGlobalComponent } from './valocity-global.component';
+
+describe('ValocityGlobalComponent', () => {
+  let component: ValocityGlobalComponent;
+  let fixture: ComponentFixture<ValocityGlobalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ValocityGlobalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ValocityGlobalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/valocity-global/valocity-global.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-valocity-global',
+  templateUrl: './valocity-global.component.html',
+  styleUrls: ['./valocity-global.component.scss']
+})
+export class ValocityGlobalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
